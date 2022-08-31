ff3e827d07fb350f4ed78df935ad108643be00bf
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-08-31 00:17:29 -0500

    Forms

--- src/main/java/com/example/springform/controller/FormController.java
+++ src/main/java/com/example/springform/controller/FormController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class FormController {
@@ -28,11 +26,6 @@ public class FormController {
         model.addAttribute("titulo", "Formulario Usuarios");
 
         if (result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(error -> {
-                errores.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
 
